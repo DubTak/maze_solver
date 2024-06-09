629b16c3aa9410a10e3b577b1ed197de4d9e90a8
DubTak <gabrielhadley@gmail.com>
2024-06-08 21:01:06 -0700

    Add cell.py and Update main.py and graphics.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from graphics import Window
+from graphics import *
+from cell import *
 
 
 def main():
     win = Window(800, 600)
+    cell1 = Cell(win)
+    cell2 = Cell(win)
+    cell2.has_bottom_wall = False
+    cell1.draw(100, 100, 200, 200)
+    cell2.draw(300, 300, 400, 400)
     win.wait_for_close()
 
 
